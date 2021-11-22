508345107cd795fd05c934f401018a52877e933c
arpanbari05 <arpanbari05@gmail.com>
2021-11-23 00:38:51 +0530

    adding header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import SortingVisualizer from "./components/SortingVisualizer";
 import classes from "./App.module.css";
+import "./utils.css";
+import "./variables.css";
+import Header from "./components/Header/Header";
+import Sortheader from "./components/Header/SortHeader";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className={classes.app}>
-      <SortingVisualizer />
-    </div>
+    <Router>
+      <Header>
+        <Sortheader />
+      </Header>
+      <div className={classes.app}>
+        <SortingVisualizer />
+      </div>
+    </Router>
   );
 }
 
